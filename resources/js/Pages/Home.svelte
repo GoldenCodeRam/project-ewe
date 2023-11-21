541c8e88f60db65ce2924ec6fce961059c0a4a62
Luis Alejandro Quiroga Gómez <lualquigo@gmail.com>
2023-11-21 08:59:51 -0500

    wip: Trying to make the scrolling work.

--- resources/js/Pages/Home.svelte
+++ resources/js/Pages/Home.svelte
@@ -6,22 +6,28 @@
     import { createProductService } from "../Functions/Services/ProductService";
 
     export let user: any;
-
     $GlobalAuthStore.user = user;
 
-    export let container: HTMLSpanElement;
+    type Page = {
+        page: number;
+        content: any[];
+    };
+
+    let container: HTMLSpanElement;
+    let scrollable: HTMLDivElement;
+
+    let data: Page[] = [];
+    let expandedData: any[] = [];
+
+    const maxDataStorage = 100;
 
     const productService = createProductService();
     const productServiceStore = productService.service.store;
     const productServiceLoadingStore = productService.service.loadingStore;
 
-    let data: any = [];
-    let test = 1;
-
     const observer = new IntersectionObserver(
         (entries) => {
             if (entries[0].isIntersecting) {
-                console.log($productServiceStore.next_page_url);
                 if ($productServiceStore.next_page_url) {
                     productService.paginationService.getNextPage();
                 }
@@ -29,7 +35,7 @@
         },
         {
             rootMargin: "0px",
-            threshold: 0.9,
+            threshold: 0.6,
         }
     );
 
@@ -37,14 +43,49 @@
         // Wait until the view is loaded.
         productService.service.get();
     });
-    $: if ($productServiceStore.current_page > 0) {
-        data = [...data, ...$productServiceStore.data];
 
+    $: if ($productServiceStore.current_page > 0) {
+        addPageBottom();
+        addExpandedData();
+
+        // The virtual list is too large to add more data, start to remove
+        // values.
+        if (maxDataStorage <= expandedData.length) {
+            const removed = data.shift();
+
+            expandedData.splice(0, removed?.content.length);
+            expandedData = expandedData;
+        }
+
+        observer.unobserve(container);
         if ($productServiceStore.next_page_url) {
             console.log($productServiceStore.current_page);
-            observer.unobserve(container);
             observer.observe(container);
+        } else {
         }
+    }
+
+    function pagesToContentArray() {}
+
+    function addPageBottom() {
+        const last = data[data.length - 1];
+
+        if (last) {
+            data.push({
+                page: last.page + 1,
+                content: $productServiceStore.data,
+            });
+        } else {
+            data.push({
+                page: 1,
+                content: $productServiceStore.data,
+            });
+        }
+    }
+
+    function addExpandedData() {
+        expandedData.push(...$productServiceStore.data);
+        expandedData = expandedData;
     }
 </script>
 
@@ -75,18 +116,24 @@
                     </div>
                 </div>
             </div>
-            <div class="col-span-8 overflow-auto bg-gray-300">
+            <div
+                bind:this={scrollable}
+                class="col-span-8 overflow-auto bg-gray-300"
+            >
                 <div class="p-8">
                     <div class="p-4 bg-white rounded shadow relative">
-                        {#each data as test}
+                        {#each expandedData as test}
                             <h1 class="bg-red-300 h-32">
                                 {test.name}
                             </h1>
                         {/each}
-                        <span bind:this={container} />
+                        <span
+                            bind:this={container}
+                            class="absolute bottom-0 bg-yellow-300 h-[50vh]"
+                        />
                     </div>
                 </div>
             </div>
         </div>
-    </div></MainLayout
->
+    </div>
+</MainLayout>
